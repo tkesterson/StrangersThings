93483a1ec0f5c995faa9ab5019ddaab1e0fe7afc
Travis Kesterson <77496059+tkesterson@users.noreply.github.com>
2021-03-22 21:31:51 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,42 @@
+const BASE_URL = "https://strangers-things.herokuapp.com/api/2101-vpi-web-pt";
+async function populatePosts() {
+  try {
+    const url = `${BASE_URL}/posts`;
+    const response = await fetch(url);
+    const data = await response.json();
+    const posts = data.data.posts;
+    const postListElement = $("#posts");
+    //const { _id, title, description } = post;
+
+    posts.forEach((post) => {
+      postListElement.append(
+        $(`
+      <div class="post" data-id="${post._id}">
+        <h3>${post.title}</h3>
+        <p>${post.description}</p>
+        <button class="edit">EDIT</button>
+        <button class="delete">DELETE</button>
+      </div>
+      `).data("post", post)
+      );
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+populatePosts();
+//test change
+$("#posts").on("click", ".edit", function () {
+  const postElement = $(this).closet(".post");
+  const post = postElement.data("post");
+  console.log(post);
+});
+$("#posts").on("click", ".delete", function () {
+  const postElement = $(this).closet(".post");
+  const post = postElement.data("post");
+  console.log(post);
+});
+$("#post-form").on("submit", function (event) {
+  event.preventDefault();
+});
+//test change #2 grsdertesdf
