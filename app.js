fdb2c5be5fba3e0eabf71669df9ffe6c57f9e393
Travis Kesterson <77496059+tkesterson@users.noreply.github.com>
2021-03-22 21:33:31 -0400

    second test change

--- app.js
+++ app.js
@@ -25,7 +25,6 @@ async function populatePosts() {
   }
 }
 populatePosts();
-//test change
 $("#posts").on("click", ".edit", function () {
   const postElement = $(this).closet(".post");
   const post = postElement.data("post");
