72ad62ebee5b4a4db8c496fefa961177507f2055
Travis Kesterson <77496059+tkesterson@users.noreply.github.com>
2021-03-23 14:36:22 -0400

    to-do clone

--- app.js
+++ app.js
@@ -5,17 +5,27 @@ async function populatePosts() {
     const response = await fetch(url);
     const data = await response.json();
     const posts = data.data.posts;
-    const postListElement = $("#posts");
+    const postListElement = $(".current-posts");
     //const { _id, title, description } = post;
-
+    console.log(posts);
     posts.forEach((post) => {
       postListElement.append(
         $(`
       <div class="post" data-id="${post._id}">
-        <h3>${post.title}</h3>
-        <p>${post.description}</p>
-        <button class="edit">EDIT</button>
-        <button class="delete">DELETE</button>
+      <h3>
+      <span class="title">
+        ${post.title}
+      </span>
+      <span class="price">
+          ${post.price}
+        </span>
+       </h3>
+        <pre>${post.description}</pre>
+        <footer class="actions">
+          <button class="action edit">EDIT</button>
+          <button class="action delete">DELETE</button>
+        </footer>
+        
       </div>
       `).data("post", post)
       );
@@ -25,16 +35,24 @@ async function populatePosts() {
   }
 }
 populatePosts();
-$("#posts").on("click", ".edit", function () {
-  const postElement = $(this).closet(".post");
+$(".post-list").on("click", ".edit", function () {
+  const postElement = $(this).closest(".post");
   const post = postElement.data("post");
   console.log(post);
 });
 $("#posts").on("click", ".delete", function () {
-  const postElement = $(this).closet(".post");
+  const postElement = $(this).closest(".post");
   const post = postElement.data("post");
   console.log(post);
 });
 $("#post-form").on("submit", function (event) {
   event.preventDefault();
+});
+$(".left-drawer").click(function (event) {
+  if ($(event.target).hasClass("left-drawer")) {
+    $("#app").toggleClass("drawer-open");
+  }
+});
+$(".add-post").click(() => {
+  $(".modal").addClass("open");
 });
